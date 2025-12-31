f85988a01ebe3f26e161e714ac16b426869784ff
Andrea Tamm <tamm.andrea2005@gmail.com>
2025-12-31 21:54:32 +0200

    Add tags CRUD

---
+++ app/pages/admin/posts/tags/[id]/edit.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useTagsStore } from '~/store/tags';
+
+const tagsStore = useTagsStore();
+const route = useRoute();
+const router = useRouter();
+
+// Load tag
+await tagsStore.getTag(route.params.id);
+
+// Init form
+let formData = ref({
+  name: tagsStore.tag.name,
+});
+
+// Update function
+async function update() {
+  await tagsStore.updateTag(route.params.id, formData.value);
+
+  if (!tagsStore.errors) {
+    router.push('/admin/tags');
+  }
+}
+</script>
+
+<template>
+  <UPageCard class="mt-3" title="Edit Tag">
+
+    <UFormField
+      label="Name"
+      :error="tagsStore.errors?.name?.find(() => true)"
+    >
+      <UInput
+        v-model="formData.name"
+        class="w-full"
+        placeholder="Tag name"
+      />
+    </UFormField>
+
+    <UButton block @click="update">Update</UButton>
+
+  </UPageCard>
+</template>
---
+++ app/pages/admin/posts/tags/[id]/view.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { useTagsStore } from '~/store/tags';
+
+const tagsStore = useTagsStore();
+const route = useRoute();
+
+// Load tag
+await tagsStore.getTag(route.params.id);
+
+// Prepare rows for UI
+let rows = [
+  {
+    name: 'ID',
+    value: tagsStore.tag.id
+  },
+  {
+    name: 'Name',
+    value: tagsStore.tag.name
+  },
+  {
+    name: 'Created',
+    value: tagsStore.tag.created_at
+  },
+  {
+    name: 'Updated',
+    value: tagsStore.tag.updated_at
+  }
+];
+</script>
+
+<template>
+  <UPageList class="mt-3">
+    <UPageCard
+      class="rounded-none"
+      v-for="(row, index) in rows"
+      :key="index"
+    >
+      <template #body>
+        <UUser
+          :name="row.name"
+          :description="row.value"
+          size="xl"
+          class="relative"
+        />
+      </template>
+    </UPageCard>
+  </UPageList>
+</template>
